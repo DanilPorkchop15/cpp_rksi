bf7cb064b0d3d5191ffe28e16d3664d5e026b864
Danil Kostin <114452500+DanilPorkchop15@users.noreply.github.com>
2023-09-06 17:08:09 +0300

    Update 1_3.cpp

--- 1_3.cpp
+++ 1_3.cpp
@@ -6,16 +6,13 @@ using namespace std;
 int main()
 {   
     double 
-        x = 0.4e4, 
-        y = -0.875, 
-        z = -0.475e-3;
-        
+        x = 3.74e-2, 
+        y = -0.825, 
+        z = 0.16e2;
     double 
-        first = pow(abs(cos(x) - cos(y)), (1 + 2 * pow(sin(y), 2))),
-        second = 1 + z + pow(z, 2) / 2 + pow(z, 3) / 3 + pow(z, 4) / 4;
-        
-    double s = first * second;
+        first = ((1. + pow(sin(x + y), 2))/abs(x - 2. * y /(1 + pow(x, 2) * pow(y, 2))))*pow(x, abs(y)),
+        second = pow(cos(atan(1/z)), 2);
+    double s = first + second;
     cout<< s;
-
     return 0;
 }
